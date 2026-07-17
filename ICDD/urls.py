e3b7a0a19b84b1a124fde7d01dfb3e7758b98470
Michael Ofosu Koranteng <118198534+Michaelk-oranteng@users.noreply.github.com>
2026-07-17 14:43:30 +0000

    Fix root URL 404 error by adding login redirect

--- ICDD/urls.py
+++ ICDD/urls.py
@@ -2,8 +2,20 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+# Root view redirect
+def home_view(request):
+    """Redirect to login page."""
+    return redirect('login')
 
 urlpatterns = [
+    # Root path - redirect to login
+    path('', home_view, name='home'),
+    
+    # Admin
     path('admin/', admin.site.urls),
-    path('adminboard/', include('control_dashboard.urls')),
+    
+    # Your app URLs at root level
+    path('', include('control_dashboard.urls')),
 ]
