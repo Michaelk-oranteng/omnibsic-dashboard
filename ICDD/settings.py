7a153134f23f611efd531e2a52fc1b2f847c8412
Michael Ofosu Koranteng <118198534+Michaelk-oranteng@users.noreply.github.com>
2026-07-17 14:02:42 +0000

    Initial commit: Add supervisor dashboard and team performance layouts

--- ICDD/settings.py
+++ ICDD/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dj5n(^hjt9*#pyz$!25u&cl=p-cb-6v-)w=agw^s*!$64@lqu('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['michaelkoranteng1.pythonanywhere.com', 'localhost', '127.0.0.1']
 
